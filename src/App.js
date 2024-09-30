7feccbe37da861a23a28dc66ce9c8eba8c3718fe
Sudeep Kumar Srivastava <sudeepsri23@gmail.com>
2024-09-30 20:50:08 +0530

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Header from './components/Header.jsx'
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header className='flex flex-row'>
+            <img src="./less-than-solid.svg" alt="loading" className='size-6 mt-6 ml-5' />
+            <p className='text-2xl font-medium mt-5 ml-5'>Create Workorder</p>
+            <button className='h-12 w-32 bg-cyan-300 text-white font-medium rounded shadow mt-4 ml-auto mr-4'>Save</button>
+        </header>
+    )
+}
